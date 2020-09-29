950eac99828846e4170d00664798e2342d33ceb4
Vasyl <vegas.firestorm@gmail.com>
2020-09-29 11:56:20 +0300

    Update settings.py

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
+# https://docs.djangoproject.com/en/2.2/topics/i18n/ d
 
 LANGUAGE_CODE = 'en-us'
 
